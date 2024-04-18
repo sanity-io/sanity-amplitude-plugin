fe1c1d04f71e9c7c359adcf3e50c26963bc6bab0
Thomas Cristina de Carvalho <thomas@frontvibe.com>
2024-04-18 09:58:24 -0400

    Project init

---
+++ packages/sanity-amplitude-experiment/src/index.ts
@@ -0,0 +1,26 @@
+import {definePlugin} from 'sanity'
+
+interface MyPluginConfig {
+  /* nothing here yet */
+}
+
+/**
+ * Usage in `sanity.config.ts` (or .js)
+ *
+ * ```ts
+ * import {defineConfig} from 'sanity'
+ * import {myPlugin} from 'sanity-plugin-sanity-amplitude-experiment'
+ *
+ * export default defineConfig({
+ *   // ...
+ *   plugins: [myPlugin()],
+ * })
+ * ```
+ */
+export const myPlugin = definePlugin<MyPluginConfig | void>((config = {}) => {
+  // eslint-disable-next-line no-console
+  console.log('hello from sanity-plugin-amplitude-experiment')
+  return {
+    name: 'sanity-plugin-amplitude-experiment',
+  }
+})
