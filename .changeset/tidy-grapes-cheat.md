64c5d0f3fda59b27b5863153d293abadfaa90744
Cody Olsen <stipsan@gmail.com>
2025-07-10 16:04:26 +0200

    Bump `@sanity/studio-secrets`, `@sanity/ui` and fix peer deps

---
+++ .changeset/tidy-grapes-cheat.md
@@ -0,0 +1,5 @@
+---
+"@tinloof/sanity-amplitude-experiment": minor
+---
+
+Update @sanity/studio-secrets and @sanity/icons to latest majors
--- packages/sanity-amplitude-experiment/package.config.ts
+++ packages/sanity-amplitude-experiment/package.config.ts
@@ -3,7 +3,6 @@ import {defineConfig} from '@sanity/pkg-utils'
 export default defineConfig({
   dist: 'dist',
   tsconfig: 'tsconfig.dist.json',
-  external: ['@sanity/ui', 'react', 'react-dom', 'sanity', 'styled-components'],
   // Remove this block to enable strict export validation
   extract: {
     rules: {
